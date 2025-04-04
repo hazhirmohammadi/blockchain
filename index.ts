6bbb988a2c9d476e6ac9fd815f81833413c7c846
hazhirmohammadi <hazhir6690@gmail.com>
2025-04-04 11:13:38 +0330

    test average-word

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import Block from "./src/block/block.ts";
 import cryptoHash from "./src/crypto-hash/crypto-hash.ts";
-
-const blockChain = new Block({
-  timestamp: "880",
-  lastHash: "sdwe",
-  data: "q2q",
-  hash: "swew",
-});
-console.log(blockChain);
+//
+// const blockChain = new Block({
+//   timestamp: "880",
+//   lastHash: "sdwe",
+//   data: "q2q",
+//   hash: "swew",
+// });
+// console.log(blockChain);
--- src/average-work.ts
+++ src/average-work.ts
@@ -1,1 +1,31 @@
 import Blockchain from "./block-chain/blockchain.ts";
+
+const blockChain = new Blockchain();
+
+blockChain.addBlock({
+  data: "initial Block",
+});
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, avrage;
+
+const times: any[] = [];
+
+for (let i = 0; i < 100; i++) {
+  prevTimestamp = blockChain.chain[blockChain.chain.length - 1]?.timestamp;
+  blockChain.addBlock({ data: `block--${i}` });
+  nextBlock = blockChain.chain[blockChain.chain.length - 1];
+  nextTimestamp = nextBlock?.timestamp;
+
+  // @ts-ignore
+  timeDiff = nextTimestamp - prevTimestamp;
+  times.push(timeDiff);
+
+  avrage = times.reduce((total, num) => total + num) / times.length;
+
+  console.log(
+    `Time to mine block: ${timeDiff}ms. Difficulty:${nextBlock?.difficulty}. Average: ${Math.round(avrage)}ms`,
+  );
+  if (i==10){
+    break
+  }
+}
