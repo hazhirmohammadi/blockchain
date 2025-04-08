31d9a42672b82d8374eff20a02eff101a6619112
hazhirmohammadi <hazhir6690@gmail.com>
2025-04-08 11:52:43 +0330

    feat: jsDoc for Block class

--- src/core/block/block.ts
+++ src/core/block/block.ts
@@ -7,6 +7,11 @@ import { GENESIS_DATA, MINE_RATE } from "./config.ts";
 import cryptoHash from "../crypto-hash/crypto-hash.ts";
 import hexToBinary from "../hex-to-binary/hex-to-binary.ts";
 
+/**
+ * Represents a block in the blockchain.
+ * @class Block
+ * @implements BlockData
+ * */
 class Block implements BlockData {
   public timestamp: string | Date;
   public lastHash: string;
@@ -15,6 +20,17 @@ class Block implements BlockData {
   public difficulty: number;
   public nonce: number;
 
+  /**
+   * Here is an example of the block class.
+   * @param {BlockData} param0 - requirements data for a block
+   * @param {string | Date} param0.timestamp - Construction time block
+   * @param {string} param0.lastHash - last hash block
+   * @param {string} param0.hash - hash this block
+   * @param {any} param0.data - data stored in the block
+   * @param {number} param0.difficulty - block mining difficulty
+   * @param {number} param0.nonce - counter used in proof of work.
+   *
+   * */
   constructor({
     timestamp,
     lastHash,
@@ -31,10 +47,21 @@ class Block implements BlockData {
     this.nonce = nonce;
   }
 
+  /**
+   * For create a first of block chain (genesis block)
+   * */
   static genesis() {
     return new Block(GENESIS_DATA);
   }
 
+  /**
+   *  Mine a new block using the previous block and input data
+   *
+   *  @param {MineBlockData} param - includes new block data and last block data
+   *  @param {BlockData} param.lastBlock - last block in chain
+   *  @param {any} param.data - desired data stored in block
+   *  @returns Block new block
+   * */
   static mineBlock({ lastBlock, data }: MineBlockData): Block {
     let hash, timestamp;
     const lastHash: string = lastBlock.hash;
@@ -62,7 +89,16 @@ class Block implements BlockData {
       nonce,
     });
   }
-
+  /**
+   * Change the difficulty based on the time of the previous block mining.
+   *
+   * If the time between the new block mining and the previous block is more than MINE_RATE, the difficulty is reduced; otherwise, it is increased.
+   *
+   * @param {aJustDifficulty} param0 - required parameters including the previous block and the current time.
+   * @param {BlockData} param0.originalBlock - the previous block in the chain.
+   * @param {number} param0.timestamp - the current time the block was mined.
+   * @returns {number} the new difficulty.
+   */
   static aJustDifficulty({
     originalBlock,
     timestamp,
